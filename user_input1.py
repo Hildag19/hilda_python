ac0d9b5dda92120b0b329bc078a2f1c85fb4f0a7
Hildag19 <160215566+Hildag19@users.noreply.github.com>
2024-02-28 22:06:55 +0300

    Add files via upload

---
+++ user_input1.py
@@ -0,0 +1,10 @@
+name= input("Enter your name:")
+print("Your name is",name)
+
+age=input("How old are you?:")
+print("I am",age)
+ 
+location=input("Where do you live?:")
+print("I live in",location)
+
+print("Enter your name:,How old are you?:I live in:",name,age,location)
